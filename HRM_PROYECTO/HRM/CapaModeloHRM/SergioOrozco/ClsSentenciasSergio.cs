333de9134b3e4b5a75e95b205d4bd2c570772fde
Jose09011712 <jlopezt11@miumg.edu.gt>
2021-03-16 21:18:36 -0600

    Estructura del proyecto
    Esta estructura ya contiene las referencias a los objetos comunes y las referencias entre el MDI y la DLL.

---
+++ HRM_PROYECTO/HRM/CapaModeloHRM/SergioOrozco/ClsSentenciasSergio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModeloHRM.SergioOrozco
+{
+    public class ClsSentenciasSergio
+    {
+        ClsConexion Cn = new ClsConexion();
+    }
+}
